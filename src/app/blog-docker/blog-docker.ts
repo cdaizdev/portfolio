cc61985d0b8ab1de6cb06a120dadb92e536d0f8a
CDiazDev <82599146+cdaizdev@users.noreply.github.com>
2026-02-12 18:19:10 +0000

    Crear blog-docker.ts

---
+++ src/app/blog-docker/blog-docker.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-blog-docker',
+  standalone: true, 
+  imports: [CommonModule],
+  templateUrl: './blog-docker.html'
+})
+export class BlogDocker {
+
+}
